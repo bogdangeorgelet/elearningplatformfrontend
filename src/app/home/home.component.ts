ebe7c0f128905d72c4d6f8bbfec8e0c0861db509
Bogdan Let <bogdangeorgelet@gmail.com>
2018-05-18 11:14:01 +0300

    added files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { AppService } from '../app-service.service';
-=======
 import { AppService } from '../app.service';
->>>>>>> ae0b8eab988b20f95066e31855ca5facdfc5c53e
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -11,16 +7,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-<<<<<<< HEAD
-export class HomeComponent implements OnInit {
-
-  title = 'Demo';
-=======
 export class HomeComponent {
   title = 'E-Learning-Platform';
->>>>>>> ae0b8eab988b20f95066e31855ca5facdfc5c53e
   greeting = {};
 
+  
   constructor(private app: AppService, private http: HttpClient) {
     http.get('http://localhost:8082/resource').subscribe(data => this.greeting = data);
   }
@@ -28,12 +19,4 @@ export class HomeComponent {
   authenticated() {
     return this.app.authenticated;
   }
-
-<<<<<<< HEAD
-  ngOnInit() {
-  }
-
-  
-=======
->>>>>>> ae0b8eab988b20f95066e31855ca5facdfc5c53e
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { AppService } from '../app-service.service';
-import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
-import { Router } from '@angular/router';
-=======
 import { AppService } from '../app.service';
 import { HttpClient } from '@angular/common/http';
 import { Route, Router } from '@angular/router';
->>>>>>> ae0b8eab988b20f95066e31855ca5facdfc5c53e
 
 @Component({
   selector: 'app-login',
@@ -15,17 +9,15 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
+
   credentials = {username: '', password: ''};
 
   constructor(private app: AppService, private http: HttpClient, private router: Router) {
   }
-  
   login() {
     this.app.authenticate(this.credentials, () => {
       this.router.navigateByUrl('/');
     })
     return false;
   }
-
 }
