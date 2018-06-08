f79a43fada5314ec5477b53fd18e48ef4aca3b14
Bogdan Let <bogdangeorgelet@gmail.com>
2018-06-08 17:30:32 +0300

    added request for submit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerComponent } from '../customer/customer.component';
 import { CustomerExample } from '../customer-example';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -9,13 +10,24 @@ import { CustomerExample } from '../customer-example';
 })
 
 export class ContactComponent {
+  private hostUrl = 'http://localhost:8082';
+  // user = new CustomerExample('Dan', 'Matei', 'danmatei@gmail.com');
+  user = {id: '', firstname: '', lastname: '', user: ''};
+  error = false;
 
-  customer = new CustomerExample('Dan', 'Matei', 'danmatei@gmail.com');
-  
+  constructor(private http: HttpClient) {}
+
   submitted = false;
 
   onSubmit() {
-    this.submitted = true;
+    const url = `${this.hostUrl}/form/add`;
+    this.error = false;
+    const body = this.user;
+    this.http.post(url, body).subscribe(data=> {
+      this.submitted = true;
+    },err => {
+      this.error = true;
+    });
   }
 
 }
