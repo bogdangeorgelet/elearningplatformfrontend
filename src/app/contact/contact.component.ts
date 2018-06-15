48126bf442ef83e84943e44e04254e421bb3da7c
Bogdan Let <bogdangeorgelet@gmail.com>
2018-06-15 14:49:10 +0300

    added contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,25 +9,40 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contact.component.css']
 })
 
-export class ContactComponent {
-  private hostUrl = 'http://localhost:8082';
-  // user = new CustomerExample('Dan', 'Matei', 'danmatei@gmail.com');
-  user = {id: '', firstname: '', lastname: '', user: ''};
+export class ContactComponent implements OnInit {
+  private hostUrl = 'http://localhost:8082/contactUs';
+  customers;
+  contactCustomer = {id: '', firstname: '', lastName: '', email: '', country: '' , phoneNumber: ''};
   error = false;
+  errorMessage;
+  submitted = false;
 
   constructor(private http: HttpClient) {}
 
-  submitted = false;
-
-  onSubmit() {
-    const url = `${this.hostUrl}/form/add`;
-    this.error = false;
-    const body = this.user;
-    this.http.post(url, body).subscribe(data=> {
-      this.submitted = true;
-    },err => {
-      this.error = true;
+  ngOnInit(): void {
+    const url = `${this.hostUrl}`;
+    this.http.get(url).subscribe(data => {
+      this.customers = data;
     });
   }
+
+  onSubmit() {
+    const url = `${this.hostUrl}/add`;
+    this.error = false;
+    const body = this.contactCustomer;
+    this.http.post(url, body).subscribe(data => {
+      this.refreshList();
+    },
+      err => {
+        this.error = true;
+        this.errorMessage = err.error.message;
+      })
+    }
+
+    refreshList() {
+      this.http.get(this.hostUrl).subscribe(data => {
+        this.customers = data;
+      })
+    }
 
 }
