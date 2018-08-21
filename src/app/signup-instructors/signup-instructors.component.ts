059981b6da5300e552de2670ac5168f2d44de867
Bogdan Let <bogdangeorgelet@gmail.com>
2018-08-21 13:52:55 +0300

    started implementation of register form

--- src/app/signup-instructors/signup-instructors.component.ts
+++ src/app/signup-instructors/signup-instructors.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-signup-instructors',
+  selector: 'signup-instructors',
   templateUrl: './signup-instructors.component.html',
   styleUrls: ['./signup-instructors.component.css']
 })
 export class SignupInstructorsComponent implements OnInit {
+  private hostUrl = 'http://localhost:8082/registerInstructor';
+  instructors;
+  registerInstructor = {id: '', firstName: '', lastName: '', email: '', username: '', password: '', confirmPassword: ''};
+  error = false;
+  classes;
+  errorMessage;
+  submitted = false;
+  showSuccessMessage = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const url = `${this.hostUrl}`;
+    this.http.get(url).subscribe(data => {
+      this.instructors = data;
+    });
+  }
+
+  onSubmit() {
+    const url = `${this.hostUrl}/add`;
+    this.error = false;
+    const body = this.registerInstructor;
+    this.http.post(url, body).subscribe(data => {
+      console.log(data);
+    },
+      err => {
+        this.showSuccessMessage = true;
+        this.error = true;
+        this.errorMessage = err.error.message;
+      })
   }
 
 }
